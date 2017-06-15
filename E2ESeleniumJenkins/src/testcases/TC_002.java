3bedfaa473d7998f3e49bf375646ab1f92d6f34d
cess8cess <cess8cess@yahoo.com>
2017-06-15 16:14:03 +0100

    aa

--- E2ESeleniumJenkins/src/testcases/TC_002.java
+++ E2ESeleniumJenkins/src/testcases/TC_002.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class TC_002 {
 
 	@Test
-	@org.testng.annotations.Test
 	public void testCase1() {
 		System.setProperty("webdriver.chrome.driver", "/usr/local/Cellar/chromedriver/2.30/bin/chromedriver");
 		ChromeDriver driver = new ChromeDriver();
